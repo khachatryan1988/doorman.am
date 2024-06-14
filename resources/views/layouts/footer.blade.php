a004e11c00ec19c0657128760dae697b238d9013
KH KH <kh.kh@dg.dc>
2024-06-14 13:58:07 +0400

    fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,46 +1,35 @@
-{{--<!doctype html>--}}
-{{--<html lang="eng">--}}
-{{--<head>--}}
-{{--    <meta charset="UTF-8">--}}
-{{--    <meta name="viewport"--}}
-{{--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">--}}
-{{--    <meta http-equiv="X-UA-Compatible" content="ie=edge">--}}
-{{--    <title>Document</title>--}}
-{{--</head>--}}
-{{--<body>--}}
+
 <footer class="footer-area section-gap">
     <div class="container">
-        <div class="row"  style="display: flex; justify-content: flex-end;">
+        <div class="row" style="display: flex; justify-content: flex-end;">
             <div class="col-lg-2">
                 <div class="single-footer-widget">
-{{--                    <h6></h6>--}}
                     <ul>
                         <li><a href="/">{{__('messages.Գլխավոր էջ')}}</a></li>
-{{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Brand Assets</a></li>--}}
-{{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Investor Relations</a></li>--}}
-{{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Terms of Service</a></li>--}}
+                        {{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Brand Assets</a></li>--}}
+                        {{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Investor Relations</a></li>--}}
+                        {{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Terms of Service</a></li>--}}
                     </ul>
                 </div>
             </div>
             <div class="col-lg-2">
                 <div class="single-footer-widget">
-{{--                    <h6></h6>--}}
                     <ul>
                         <li><a href="{{ route('about') }}">{{__('messages.Մեր Մասին')}}</a></li>
-{{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Brand Assets</a></li>--}}
-{{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Investor Relations</a></li>--}}
-{{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Terms of Service</a></li>--}}
+                        {{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Brand Assets</a></li>--}}
+                        {{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Investor Relations</a></li>--}}
+                        {{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Terms of Service</a></li>--}}
                     </ul>
                 </div>
             </div>
             <div class="col-lg-2">
                 <div class="single-footer-widget">
-{{--                    <h6><a href="">{{__('messages.Ծառայություններ')}}</a></h6>--}}
-                    <ul>
-                        <li><a href="{{ route('service') }}">{{__('messages.Ծառայություններ')}}</a></li>
-{{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Research</a></li>--}}
-{{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Experts</a></li>--}}
-{{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Agencies</a></li>--}}
+                     <p><a href="{{ route('service') }}">{{__('messages.Ծառայություններ')}}</a></p>
+
+                    <ul class="custom-list">
+{{--                        <li><a href="{{ route('service') }}">{{__('messages.Ծառայություններ')}}</a></li>--}}
+                        <li><a href="{{ route('measurement') }}">{{__('messages.Չափագրում')}}</a></li>
+                        <li><a href="{{ route('consultation') }}">{{__('messages.Խորհրդատվություն')}}</a></li>
                     </ul>
                 </div>
             </div>
@@ -55,47 +44,34 @@
                     </ul>
                 </div>
             </div>
-{{--            <div class="col-lg-3">--}}
-{{--                <div class="single-footer-widget">--}}
-{{--                    <h6><a href="{{ route('galery') }}">{{__('messages.Մեր Աշխատանքները')}}</a></h6>--}}
-{{--                    <p>Let us be social</p>--}}
-{{--                    <div class="footer-social d-flex align-items-center">--}}
-{{--                        <a href="https://preview.colorlib.com/theme/taxi/index.html#"><i class="fa fa-facebook"></i></a>--}}
-{{--                        <a href="https://preview.colorlib.com/theme/taxi/index.html#"><i class="fa fa-twitter"></i></a>--}}
-{{--                        <a href="https://preview.colorlib.com/theme/taxi/index.html#"><i class="fa fa-dribbble"></i></a>--}}
-{{--                        <a href="https://preview.colorlib.com/theme/taxi/index.html#"><i class="fa fa-behance"></i></a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+            {{--            <div class="col-lg-3">--}}
+            {{--                <div class="single-footer-widget">--}}
+            {{--                    <h6><a href="{{ route('galery') }}">{{__('messages.Մեր Աշխատանքները')}}</a></h6>--}}
+            {{--                    <p>Let us be social</p>--}}
+            {{--                    <div class="footer-social d-flex align-items-center">--}}
+            {{--                        <a href="https://preview.colorlib.com/theme/taxi/index.html#"><i class="fa fa-facebook"></i></a>--}}
+            {{--                        <a href="https://preview.colorlib.com/theme/taxi/index.html#"><i class="fa fa-twitter"></i></a>--}}
+            {{--                        <a href="https://preview.colorlib.com/theme/taxi/index.html#"><i class="fa fa-dribbble"></i></a>--}}
+            {{--                        <a href="https://preview.colorlib.com/theme/taxi/index.html#"><i class="fa fa-behance"></i></a>--}}
+            {{--                    </div>--}}
+            {{--                </div>--}}
+            {{--            </div>--}}
             <div class="col-lg-2">
                 <div class="single-footer-widget">
-                    <ul>
-                        <li><a href="{{ route('contactUs') }}">{{__('messages.Կապ')}}</a></li>
-                        {{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Research</a></li>--}}
-                        {{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Experts</a></li>--}}
-                        {{--                        <li><a href="https://preview.colorlib.com/theme/taxi/index.html#">Agencies</a></li>--}}
+                    <p><a href="{{ route('contactUs') }}">{{__('messages.Կապ')}}</a></p>
+                    <ul class="custom-list">
+                        <li><a href="mailto:{{ config('app.contacts.email') }}">{{ config('app.contacts.email') }}</a>
+                        </li>
+                        <li>
+                            <a href="tel:{{ config('app.contacts.phone_number') }}">{{ config('app.contacts.phone_number') }}</a>
+                        </li>
                     </ul>
-{{--                    <h6><a href="https://preview.colorlib.com/theme/taxi/contact.html">{{__('messages.Կապ')}}</a></h6>--}}
-{{--                    <p>Stay update with our latest</p>--}}
-{{--                    <div class="" id="mc_embed_signup">--}}
-{{--                        <form target="_blank"--}}
-{{--                              action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"--}}
-{{--                              method="get" class="form-inline" novalidate="true">--}}
-{{--                            <input class="form-control" name="EMAIL" placeholder="Enter Email"--}}
-{{--                                   onfocus="this.placeholder = &#39;&#39;"--}}
-{{--                                   onblur="this.placeholder = &#39;Enter Email &#39;" required="" type="email">--}}
-{{--                            <button class="click-btn btn btn-default"><span class="lnr lnr-arrow-right"></span></button>--}}
-{{--                            <div style="position: absolute; left: -5000px;">--}}
-{{--                                <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">--}}
-{{--                            </div>--}}
-{{--                            <div class="info"></div>--}}
-{{--                        </form>--}}
-{{--                    </div>--}}
                 </div>
             </div>
             <p class="mt-40 mx-auto footer-text col-lg-12">
 
-                Copyright © 2024 - <script>document.write(new Date().getFullYear() !== 2024 ? new Date().getFullYear() : '');</script>
+                Copyright © 2024 -
+                <script>document.write(new Date().getFullYear() !== 2024 ? new Date().getFullYear() : '');</script>
                 All rights reserved | <a href="https://doorman.am/">Doorman</a>
 
             </p>
@@ -103,6 +79,5 @@
     </div>
     <img class="footer-bottom" src="{{ asset('images/footer-bottom.png') }}" alt="">
 </footer>
-{{--</body>--}}
-{{--</html>--}}
+
 
