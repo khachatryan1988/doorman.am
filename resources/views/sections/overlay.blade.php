5c754741d3782ed42e78adc7c5646cdce81156b9
KH KH <kh.kh@dg.dc>
2025-02-19 18:06:46 +0400

    fix

--- resources/views/sections/overlay.blade.php
+++ resources/views/sections/overlay.blade.php
@@ -85,16 +85,16 @@
                         <!-- Square Meter Calculation Form -->
                         <div class="metrica-style-container">
                             <strong><h2 style="font-size: 20px; color:#FDBD1D;">Հաշվել Դարպասի Արժեքը</h2></strong>
-{{--                            <form action="{{ route('calculate.m2') }}" method="POST">--}}
-{{--                                @csrf--}}
-{{--                                <label style="color: white;" for="width">Լայնություն (մ):</label>--}}
-{{--                                <input  type="number" name="width" id="width" step="0.01" required>--}}
-{{--                                <label style="color: white;" for="height">Բարձրություն (մ):</label>--}}
-{{--                                <input type="number" name="height" id="height" step="0.01" required>--}}
-{{--                                <div class="button-container">--}}
-{{--                                    <button type="submit">Հաշվել</button>--}}
-{{--                                </div>--}}
-{{--                            </form>--}}
+                            <form action="{{ route('calculate.m2') }}" method="POST">
+                                @csrf
+                                <label style="color: white;" for="width">Լայնություն (մ):</label>
+                                <input  type="number" name="width" id="width" step="0.01" required>
+                                <label style="color: white;" for="height">Բարձրություն (մ):</label>
+                                <input type="number" name="height" id="height" step="0.01" required>
+                                <div class="button-container">
+                                    <button type="submit">Հաշվել</button>
+                                </div>
+                            </form>
                             @if(session('result'))
                                 <div class="result-box">
                                     <p>Քառակուսի մետր: <span>{{ session('result') }} մ²</span></p>
